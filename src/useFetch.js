f358bcb0f7b66e018cfd36a9fd06dc036a45cc18
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-01-05 14:21:03 -0300

    test

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,25 +1,25 @@
 import { useState, useEffect } from 'react'
 
-// const url =
-//   "https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=5";
+const url =
+  'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=5'
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-//   },
-// }
-// export function useFetch(url) {
-//   const [data, setData] = useState(null)
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '23a37debadmsh89268f610a958f7p1ce41cjsnb0abdc7f3b4c',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+}
+export function useFetch(url) {
+  const [data, setData] = useState(null)
 
-//   useEffect(() => {
-//     fetch(url, options)
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setData(data)
-//       })
-//   }, [])
+  useEffect(() => {
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+      })
+  }, [])
 
-//   return { data }
-// }
+  return { data }
+}
