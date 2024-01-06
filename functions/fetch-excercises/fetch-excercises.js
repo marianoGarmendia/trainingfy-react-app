8578cbd739c1e7a7851ed8a3123983d78a25f1c3
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-01-05 21:29:31 -0300

    config fetch functions netlify

--- functions/fetch-excercises/fetch-excercises.js
+++ functions/fetch-excercises/fetch-excercises.js
@@ -1,17 +1,22 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// const API_KEY = "23a37debadmsh89268f610a958f7p1ce41cjsnb0abdc7f3b4c"
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '23a37debadmsh89268f610a958f7p1ce41cjsnb0abdc7f3b4c',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+}
+
 const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
+  const { url } = event.queryStringParameters.name
+  const data = await fetch(url, options)
+    .then((res) => res.json())
+    .then((response) => {
+      return response
+    })
+
+  return data
 }
 
 module.exports = { handler }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,25 +1,23 @@
 import { useState, useEffect } from 'react'
 
-const url =
-  'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=5'
+// const url =
+//   'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=5'
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '23a37debadmsh89268f610a958f7p1ce41cjsnb0abdc7f3b4c',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-  },
-}
 export function useFetch(url) {
   const [data, setData] = useState(null)
 
   useEffect(() => {
-    fetch(url, options)
+    fetch(`/.netlify/functions/fetch-excercises?name=${url}`)
       .then((res) => res.json())
-      .then((data) => {
-        setData(data)
-      })
+      .then((response) => setData(response))
   }, [])
+  // useEffect(() => {
+  //   fetch(url, options)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data)
+  //     })
+  // }, [])
 
   return { data }
 }
