55ede34fc66c7c937bd87822c02fe251e7d849bd
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-07-01 09:57:25 -0300

    options cookies modifided

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -56,7 +56,7 @@ authRouter.post('/login', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+
       sameSite: 'strict',
     })
 
