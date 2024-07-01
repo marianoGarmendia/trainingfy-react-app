1be7d640440d168337ee9a8b25236f33debe0ae0
Mariano Garmendia <marianogarmendia77@gmail.com>
2024-07-01 09:12:43 -0300

    port added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import app from './app.js'
-
-app.listen(3000, () => {
-  console.log('Escuchando en el puerto 3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  // console.log('Escuchando en el puerto 3000')
 })
